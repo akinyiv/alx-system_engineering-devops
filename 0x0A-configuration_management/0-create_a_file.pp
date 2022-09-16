56f41a7f3f8f79753c8f0f1bf9b0db2d935b9789
akinyiv <akinyivee10@gmail.com>
2022-09-16 18:38:02 +0300

    create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,7 @@
-# creates a file in the /tmp directory
-file { '/tmp/holberton':
-  ensure  => 'file',
+# create a file in /tmp with specific requirements
+
+file { 'holberton':
+  path    => '/tmp/holberton',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
